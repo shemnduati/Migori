3a70157ac6d84b61acb30b3778797f297c53fc3e
Ochieng424 <ochiengderrick424@gmail.com>
2019-08-09 14:47:07 +0300

    created application component

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,11 +7,12 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
 let routes = [
     { path: '/dashboard', component: require('./components/dashboard.vue').default },
+    { path: '/application', component: require('./components/Application.vue').default },
 ]
 const router = new VueRouter({
     mode:'history',
