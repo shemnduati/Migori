9b69352ade769be7107ac81f8591bd1d276a4fdb
Ochieng424 <ochiengderrick424@gmail.com>
2019-08-13 09:01:48 +0300

    :art: added api token to models

--- app/Geographical.php
+++ app/Geographical.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class Geographical extends Model
 {
+    use HasApiTokens;
     protected $guarded= [];
     protected $table = 'geographical';
 }
--- app/Institution.php
+++ app/Institution.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class Institution extends Model
 {
+    use HasApiTokens;
     protected $guarded= [];
     protected $table = 'institution';
 }
