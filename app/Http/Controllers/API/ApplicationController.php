bc57ef8af9ec61a686c421d1ac557ba1473ce121
shemnduati <nduatishem@gmail.com>
2019-09-14 20:19:56 +0300

    corrected the bug in application controller getwards function

--- app/Http/Controllers/API/ApplicationController.php
+++ app/Http/Controllers/API/ApplicationController.php
@@ -38,9 +38,6 @@ class ApplicationController extends Controller
 
     public function getWards()
     {
-        $apps= Application::where('status', 0)->where('user_id', Auth::user()->id)->count();
-        return view('layout.dashboard')->with('apps', $apps);
-
         $wards = Ward::all();
 
         return ['wards'=>$wards];
