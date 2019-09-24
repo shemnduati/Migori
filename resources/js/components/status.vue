af40230f685bf8a39f6594e296a75b65cecc6dfa
shemnduati <nduatishem@gmail.com>
2019-09-24 10:10:55 +0300

    working on application status

--- resources/js/components/status.vue
+++ resources/js/components/status.vue
@@ -1,12 +1,27 @@
 <template>
-    <div class="container">
+    <div class="container" v-if="count > 0">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Application Status</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        <table class="table table-hover">
+                            <tbody><tr>
+                                <th>Status</th>
+                            </tr>
+                            <tr v-for="application in applications" :key="application.id">
+                                <td>
+                                    <span v-if="application.status==0" style="color: purple;">Received</span>
+                                    <span v-if="application.status==2" style="color: red;">Rejected</span>
+                                    <span v-if="application.status==3" style="color: green;">Verified</span>
+                                </td>
+                                <td>Read More</td>
+                            </tr>
+
+
+
+                            </tbody></table>
                     </div>
                 </div>
             </div>
@@ -16,8 +31,23 @@
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        data(){
+            return {
+                applications :{},
+                count : {},
+            }
+        },
+        methods:{
+            getApplications(){
+                    axios.get('api/getMyBursary').then(({data}) => ([this.applications = data['applications']]));
+            },
+            getMyApplications(){
+                axios.get('api/getApplication').then(({data}) => ([this.count = data['count']]));
+            },
+        },
+        created() {
+            this.getApplications();
+            this. getMyApplications();
         }
     }
 </script>
