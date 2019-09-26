e77a7236a74249fa1cd5976e3fa8c3ff043346a6
shemnduati <nduatishem@gmail.com>
2019-09-26 15:28:57 +0300

    removed extra terms link on dashoard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -201,14 +201,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </router-link>
 
                     @endcan
-                    <li class="nav-item">
-                        <router-link to="/privacy" class="nav-link">
-                            <i class="nav-icon fas fa-clipboard-list cyan"></i>
-                            <p>
-                               Terms
-                            </p>
-                        </router-link>
-                    <li class="nav-item">
                         <router-link to="/profile" class="nav-link">
                             <i class="nav-icon fa fa-user orange"></i>
                             <p>
