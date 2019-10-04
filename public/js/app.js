12c29d25b04bfb507ee84c824b29b488c845fff9
jeremihz <mihjere68@gmail.com>
2019-10-04 13:57:45 +0300

    file

--- public/js/app.js
+++ public/js/app.js
@@ -97671,13 +97671,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 __webpack_require__(/*! C:\wamp64\www\Baza\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\wamp64\www\Baza\resources\sass\app.scss */"./resources/sass/app.scss");
-=======
-__webpack_require__(/*! /opt/lampp/htdocs/Transonline/Baza/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/Transonline/Baza/resources/sass/app.scss */"./resources/sass/app.scss");
->>>>>>> 0599a9e269b6ba8eebfc761d3c64ec78bd393e58
 
 
 /***/ })
