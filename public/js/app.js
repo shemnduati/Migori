7af45a6bf6f3dc3aa7845bb09a85face11cf3f83
Ochieng424 <ochiengderrick424@gmail.com>
2019-12-01 14:23:56 +0300

    added awarded amount in budgets

--- public/js/app.js
+++ public/js/app.js
@@ -9443,6 +9443,8 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
@@ -94189,6 +94191,13 @@ var render = function() {
                               _vm._v("Ksh. " + _vm._s(budget.remaining))
                             ]),
                             _vm._v(" "),
+                            _c("td", [
+                              _vm._v(
+                                "Ksh. " +
+                                  _vm._s(budget.amount - budget.remaining)
+                              )
+                            ]),
+                            _vm._v(" "),
                             _c("td", [_vm._v(_vm._s(budget.year))]),
                             _vm._v(" "),
                             _c("td", [
@@ -94475,6 +94484,8 @@ var staticRenderFns = [
       _c("th", [_vm._v("Budget")]),
       _vm._v(" "),
       _c("th", [_vm._v("Current Balance")]),
+      _vm._v(" "),
+      _c("th", [_vm._v("Awarded Amount")]),
       _vm._v(" "),
       _c("th", [_vm._v("Budget year")]),
       _vm._v(" "),
