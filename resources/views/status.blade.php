a82e854abdccba11a34750129c53c5cd21537c8c
jeremihz <mihjere68@gmail.com>
2020-03-02 18:00:19 +0300

    bug

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -3,13 +3,12 @@
 @section('content')
     <div class="grad col-md-12 pb-3">
         <div class="text-center pt-4 baza">
-            <h3 class="font-weight-bold">BURSARY APPLICATION SYSTEM</h3>
+            <h3 class="font-weight-bold">MIGORI COUNTY GOVERNMENT</h3>
         </div>
 
 
         <div class="text-center pt-2">
-            <h3 class="font-weight-bolder big"><span class="b">B </span><span class="a">A </span><span
-                        class="z">Z </span><span class="x">A</span></h3>
+           
             <a href="/">
                 <button class="btn btn-primary">
                     <i class="fa fa-hand-point-left"></i>
