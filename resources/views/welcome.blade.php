393b2c05fe5cb0a5135fa4175666801f401a2440
kollynce <collinsodeny@gmail.com>
2020-05-19 10:35:54 +0300

    status added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,6 +50,7 @@
                                             </a>
 
                                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                                <a href="/status" class="dropdown-item">Application Status</a>
                                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                                    onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
