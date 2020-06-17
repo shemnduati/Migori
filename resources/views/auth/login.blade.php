e496debfc3e1c4445c97954174f29293447274c0
Ochieng424 <ochiengderrick424@gmail.com>
2020-06-17 12:43:21 +0300

    updating ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,12 +61,10 @@
                     <ul class="nav nav-tabs" id="myTab" role="tablist">
                         <li class="nav-item">
 
-                            <a class="nav-link active" id="login-tab" data-toggle="tab" href="#login" role="tab"
-                               aria-controls="login" aria-selected="true">Login</a>
+                            <a class="nav-link active" href="/login">Login</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="register-tab" data-toggle="tab" href="#register" role="tab"
-                               aria-controls="register" aria-selected="false">Register</a>
+                            <a class="nav-link" href="/register">Register</a>
                         </li>
                     </ul>
                     <div class="tab-content" id="myTabContent">
